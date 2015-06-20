684accd6270cee4a2b15d5bee9c2c7e825d432ed
Russ <codemunkee@gmail.com>
2015-06-20 03:06:51 +0000

    more tests for valid team names, support lower case names

--- test_EmelbeeStats.py
+++ test_EmelbeeStats.py
@@ -7,7 +7,6 @@ stats = EmelbeeStats.EmelbeeStats('2015','06','15',
 
 # print string returned from team_scores function
 def test_all_score_output():
-    #patterns = re.compile(r'\bDodgers\b' | r'\bGiants\b')   
     patterns=re.compile(r'\bPadres\b|\Giants\b|\bAstros\b|\bDodgers\b')   
     assert patterns.search(stats.team_scores())
 
@@ -15,3 +14,19 @@ def test_all_score_output():
 def test_connect_to_api():
     json = stats.return_stats()
     assert isinstance(json, dict)
+
+# make sure if we give a bogus team name the method returns false
+def test_invalid_team():
+    assert stats.valid_team('Gnats') == False
+
+# make sure if we give a valid team name the method returns false
+def test_valid_team():
+    assert stats.valid_team('Dodgers') == True
+
+# make sure if we give a valid team name the method returns false
+def test_case_insensitive_team():
+    assert stats.valid_team('doDGers') == True
+
+# get an individual team score
+def test_team_score():
+    assert re.match('Dodgers', stats.team_scores('Dodgers'))
