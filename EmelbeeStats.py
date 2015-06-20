02bb9722d6b4dc9f31695eb4fe4624ab515aab69
Russ <codemunkee@gmail.com>
2015-06-20 13:03:21 -0700

    added more code to allow more abbreviated results and better handling of lack of API results

--- EmelbeeStats.py
+++ EmelbeeStats.py
@@ -3,7 +3,7 @@
 import requests
 import json
 import sys
-
+import re
 
 class EmelbeeStats:
     """ Pull down MLB stats in JSON from MLB's (mlb.com) free API.
@@ -69,11 +69,6 @@ class EmelbeeStats:
                            'giants',
                            'mariners']
 
-    def print_json(self, json):
-        """ Pretty Print the JSON """
-        print json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4,
-                         separators=(',', ': '))
-
     def return_stats(self):
         """ Returns Stats in JSON format, get it from a local file if one
             is provided, otherwise hit the MLB API directly """
@@ -84,11 +79,23 @@ class EmelbeeStats:
             if self.debug:
                 print 'Debug: Hitting URL ' + self.assemble_url()
             api_resp = requests.get(self.assemble_url())
+            if self.debug:
+                print 'Debug: %s API Response Code' % (api_resp.status_code)
             if api_resp.status_code != requests.codes.ok:
-                sys.exit('API not responding the way we want: Response ' +
-                         'Code ' + str(api_resp.status_code))
+                sys.exit(self.no_game_info_found())
             else:
                 return api_resp.json()
+
+    def no_game_info_found(self, team=None):
+        if team:
+            #return str('No game information found for %s on ' +
+            #       '%s/%s/%s' % (team.title(), self.month, self.day, self.year))
+            return str('No game information found for %s on %s/%s/%s' % (team.title(), self.month, self.day, self.year))
+        else:
+            return str('No game information found for ' +
+                   '%s/%s/%s. You sure it\'s in season?' % (self.month,
+                                                            self.day,
+                                                            self.year))
 
     def assemble_url(self):
         """ We need to assemble a URL to pull down stats with. """
@@ -107,42 +114,64 @@ class EmelbeeStats:
         """ Return Scores """
 
         # Placeholder for Scores String
-        scores = ''
+        scores = str()
 
         # Convert team name to lower case if defined
         if team:
             team = team.lower()
 
+        # If a team is defined but it's not valid.
         if team != None and not self.valid_team(team):
             sys.exit('"%s" is not a valid team name.' % team)
 
+        # Sometimes there is JSON data defined but no actual games, bail out
+        # if we run into that...
+        if not 'game' in self.json_stats['data']['games']:
+            return self.no_game_info_found()
         for stat in self.json_stats['data']['games']['game']:
             home_team = stat['home_team_name'].lower()
             away_team = stat['away_team_name'].lower()
+            home_team_abbrev = stat['home_name_abbrev']
+            away_team_abbrev = stat['away_name_abbrev']
 
-           # If it's not the home or the away team defined, and one
-           # is defined, we try the next
+            # If it's not the home or the away team defined, and one
+            # is defined, we try the next
             if (home_team != team and away_team != team) and team != None:
                  continue
 
-            # The note summarizes the score 
-            note = stat['alerts']['brief_text']
-            scores = scores + note + '\n'
+            # Game Status
+            game_status = stat['status']['status']
+            if self.debug:
+                print 'Debug: %s' % game_status
 
-            ## Go through the stats file
-            #for item in stat.items():
-            #    # Get the Line Score
-            #    if item[0] == 'linescore':
-            #        home_score = stat[item[0]]['r']['home']
-            #        away_score = stat[item[0]]['r']['away']
+            try:
+                # The note summarizes the score 
+                note = stat['alerts']['brief_text']
+                scores = scores + note + '\n'
+ 
+            except KeyError:
 
-            #scores = scores + '%s (GB:%s) @ %s (GB:%s) :: '\
-            #                  '(%s-%s) %s\n' % (away_team.title(),
-            #                                    away_team_gb,
-            #                                    home_team.title(),
-            #                                    home_team_gb,
-            #                                    away_score,
-            #                                    home_score,
-            #                                    game_status)
+                if 'linescore' in stat.keys():
+                    home_score = stat['linescore']['r']['home']
+                    away_score = stat['linescore']['r']['away']
+                else:
+                    home_score = 0
+                    away_score = 0
+
+                # If the game hasn't start, get the start time
+                if re.search('Preview', game_status):
+                    home_time = stat['home_time']
+                    home_tz = stat['home_time_zone']
+                    game_status = '%s %s' % (home_time, home_tz)
+
+                scores = scores + '%s @ %s (%s-%s) - %s\n' % (away_team_abbrev,
+                                                              home_team_abbrev,
+                                                              away_score,
+                                                              home_score, game_status)
+                 
+        if not scores and team:
+            return self.no_game_info_found(team)
+        elif not scores:
+            return self.no_game_info_found()
 
         return scores.rstrip()
