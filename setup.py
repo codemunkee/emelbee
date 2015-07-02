8bcf8569f79af1c83adc6bc5ef99149e36b00949
Russ Nealis <rnealis@twilio.com>
2015-07-01 20:41:08 -0700

    simplified setup to only incude the one module

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='EmelbeeStats',
-      version='0.3',
+      version='0.3.1',
       description='Returns MLB Scores and Standings by querying APIs',
       url='https://github.com/codemunkee/emelbee',
       author='Russ',
       author_email='codemunkee@gmail.com',
       license='BSD',
-      packages = ['EmelbeeStats'],
+      py_modules = ['EmelbeeStats'],
       scripts = ['bin/get_stats', 'bin/emelbee_api'],
       install_requires=['requests>=2.7.0'],
       zip_safe=False)
