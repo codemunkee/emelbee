fee28b790a4502994ddb440d22a65a46fddfb5df
Russ Nealis <rnealis@twilio.com>
2015-07-06 14:00:05 -0700

    Added tests for standings, also look old stats and todays stats

--- tests/test_EmelbeeStats.py
+++ tests/test_EmelbeeStats.py
@@ -3,6 +3,7 @@ import re
 import sys
 import os
 import inspect
+from datetime import date
 
 
 # This lets us load the module from the parent directory
@@ -10,49 +11,61 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
+today = date.today()
+year = today.strftime('%Y')
+month = today.strftime('%m')
+day = today.strftime('%d')
+
 import EmelbeeStats
 
-stats = EmelbeeStats.EmelbeeStats('2015', '07', '01',
-                                  'data/scores.json')
+past_stats = EmelbeeStats.EmelbeeStats('2015', '07', '01',
+                                       'data/scores.json')
+
+todays_stats = EmelbeeStats.EmelbeeStats(year, month, day)
 
 
-# print string returned from team_scores function
-def test_all_score_output():
+
+# print string returned from team_scores function (old stats)
+def test_pastday_all_score_output():
     patterns = re.compile(r'\bLAD\b|\SF\b|\CWS\b|\bBAL\b')
-    assert patterns.search(stats.team_scores())
+    assert patterns.search(past_stats.team_scores())
 
+# print string returned from team_scores function (today stats)
+def test_todays_all_score_output():
+    patterns = re.compile(r'\bLAD\b|\SF\b|\CWS\b|\bBAL\b')
+    assert patterns.search(past_stats.team_scores())
 
-# connect to the API and get JSON dict results from scores function
-def test_connect_to_api():
-    json = stats.return_scores()
-    assert isinstance(json, dict)
-
+# print string returned from team_scores function (today stats)
+def test_todays_all_standings_output():
+    patterns = re.compile(r'\bDiamondbacks\b|\Giants\b')
+    assert patterns.search(past_stats.team_standings('nl', 'w'))
 
 # make sure if we give a bogus team name the method returns false
 def test_invalid_team():
-    assert stats.valid_team('Gnats') == False
+    assert past_stats.valid_team('Gnats') == False
 
 
 # make sure if we give a valid team name the method returns false
 def test_valid_team():
-    assert stats.valid_team('Dodgers') == True
+    assert past_stats.valid_team('Dodgers') == True
 
 
 # make sure if we give a valid team name the method returns false
 def test_case_insensitive_team():
-    assert stats.valid_team('doDGers') == True
+    assert past_stats.valid_team('doDGers') == True
 
 
 # get an individual team score
 def test_team_score():
-    assert re.match('LAD|Dodgers', stats.team_scores('Dodgers'))
+    assert re.match('LAD|Dodgers', past_stats.team_scores('Dodgers'))
 
 # get scores from cache
 def test_scores_from_cache_file():
-    scores = stats.read_cache('tests/emelbee_scores_cache.json')
+    scores = past_stats.read_cache('tests/emelbee_scores_cache.json')
     assert isinstance(scores, dict)
 
 # get standings from cache
 def test_standings_from_cache_file():
-    standings = stats.read_cache('tests/emelbee_standings_cache.json')
+    standings = past_stats.read_cache('tests/emelbee_standings_cache.json')
     assert isinstance(standings, dict)
+
